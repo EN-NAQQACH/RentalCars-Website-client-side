1b0b3d108d7cd77136df7b0c3d6304747b15690c
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-04-11 20:10:10 +0000

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Main from './components/Main'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar />
+      <Hero />
+      <Main />
     </>
   )
 }
