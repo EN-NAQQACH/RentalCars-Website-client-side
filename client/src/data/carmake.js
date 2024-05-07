6eb32f49006325a9cd70f4ec0d0fbeb566817370
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-05-07 02:22:30 +0100

    changes

---
+++ client/src/data/carmake.js
@@ -0,0 +1,26 @@
+const carmake =[
+    "Toyota",
+    "Fait",
+    "Dacia",
+    "Ford",
+    "Volkswagen",
+    "Honda",
+    "Chevrolet",
+    "Nissan",
+    "BMW",
+    "Mercedes-Benz",
+    "Audi",
+    "Hyundai",
+    "Kia",
+    "Volvo",
+    "Subaru",
+    "Mazda",
+    "Lexus",
+    "Jeep",
+    "Tesla",
+    "Ferrari",
+    "Porsche",
+    "Mitsubishi",
+    "Others"
+].sort();
+export default carmake
